880a96468b35530b45d1282d611095dfd784d88c
ida <ida@MBPsomtllhorida.lan>
2022-12-13 08:52:09 +0100

    Day one and two

---
+++ day-one/calorie-counting.js
@@ -0,0 +1,43 @@
+const fs = require('fs/promises');
+
+async function example() {
+    const data = await fs.readFile('data.txt', { encoding: 'utf8' });
+    return data;
+}
+
+(async ()=>{
+    const calories = await example();
+
+    //split elves into different strings
+    const caloriesArray = calories.split('\n\n');
+
+     //create an array that will store the sum of each elf's calories beginning from the one with the least amount
+    const sortedCaloriesArray = [];
+
+    //count the sum of one elf's calories
+    const sumArray = (array) => {
+        let sum = 0;
+        for (let i in array) {
+            sum += array[i];
+        }
+        sortedCaloriesArray.push(sum);
+    }
+
+    for (let i in caloriesArray) {
+        //put one elf's calories into one row and make the calories strings into numbers
+        caloriesArray[i] = caloriesArray[i]
+        .split('\n')
+        .map(x => Number(x));
+
+        //count the sum of one elfs calories
+        sumArray(caloriesArray[i]);
+    }
+
+    //sort which elf has the highest amount of calories
+    sortedCaloriesArray.sort((a,b)=>a-b);
+
+    console.log(sortedCaloriesArray[sortedCaloriesArray.length - 1]);
+    console.log(sortedCaloriesArray[sortedCaloriesArray.length - 2]);
+    console.log(sortedCaloriesArray[sortedCaloriesArray.length - 3]);
+    }
+)();
---
+++ day-two/testing-strategy.js
@@ -0,0 +1,81 @@
+const fs = require('fs/promises');
+
+async function example() {
+    const data = await fs.readFile('data.txt', { encoding: 'utf8' });
+    return data;
+}
+
+(async ()=>{
+    const guide = await example();
+
+    //split data rows into arrays and replace letters with their meaning
+    let strategyOneArray = guide.split('\n')
+    .map(letter => letter.replace('A', 'rock')
+    .replace('B', 'paper')
+    .replace('C', 'scissors')
+    .replace('X', 'rock')
+    .replace('Y', 'paper')
+    .replace('Z', 'scissors'));
+
+    //counter for the score
+    let scorePartOne = 0;
+    let scorePartTwo = 0;
+
+    for (let i in strategyOneArray) {
+        //delete spacing
+        strategyOneArray[i] = strategyOneArray[i].split(' ');
+
+        //set first element in the array to the competitor weapon
+        let competitorWeapon = strategyOneArray[i][0];
+
+        //set second element in the array to the chosen weapon
+        let chosenWeapon = strategyOneArray[i][1];
+
+        //set score for draw and wins
+        const drawScore = 3;
+        const winningScore = 6;
+
+        //
+        const weaponScore = (weapon) => {
+            const chosenWeaponScore = chosenWeapon === 'rock' ? scorePartOne = scorePartOne + 1
+                : chosenWeapon === 'paper' ? scorePartOne = scorePartOne + 2
+                : chosenWeapon === 'scissors' ? scorePartOne = scorePartOne + 3
+                : console.log('Error');
+        }
+
+        if (competitorWeapon === chosenWeapon) {
+            scorePartOne = scorePartOne + drawScore;
+        } 
+
+        if (competitorWeapon === 'rock' && chosenWeapon === 'paper') { 
+            scorePartOne = scorePartOne + winningScore;
+            scorePartTwo = scorePartTwo + drawScore + 1;
+        } else if (competitorWeapon === 'paper' && chosenWeapon === 'scissors' ) {
+            scorePartOne = scorePartOne + winningScore;
+            scorePartTwo = scorePartTwo + winningScore + 3;
+        } else if (competitorWeapon === 'scissors' && chosenWeapon === 'rock') {
+            scorePartOne = scorePartOne + winningScore;
+            scorePartTwo = scorePartTwo + 2;
+        } else if (competitorWeapon === 'rock' && chosenWeapon === 'scissors') {
+            scorePartTwo = scorePartTwo + winningScore + 2;
+        } else if (competitorWeapon === 'rock' && chosenWeapon === 'rock') {
+            scorePartTwo = scorePartTwo + 3;
+        } else if (competitorWeapon === 'paper' && chosenWeapon === 'rock') {
+            scorePartTwo = scorePartTwo + 1;
+        } else if (competitorWeapon === 'scissors' && chosenWeapon === 'paper') {
+            scorePartTwo = scorePartTwo + drawScore + 3;
+        } else if (competitorWeapon === 'paper' && chosenWeapon === 'paper') {
+            scorePartTwo = scorePartTwo + drawScore + 2;
+        } else if (competitorWeapon === 'scissors' && chosenWeapon === 'scissors') {
+            scorePartTwo = scorePartTwo + winningScore + 1;
+        } else {
+            console.log('Something went wrong');
+        }
+
+        weaponScore(chosenWeapon);
+    }
+
+    console.log('The answer is for part one is: ' + scorePartOne);
+    console.log('The answer is for part one is: ' + scorePartTwo);
+}
+)();
